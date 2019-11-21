090c06610aa5c4291af33848b8a2af6008240eb9
Ranjith kumar <rvanaparthi@firstrepublic.com>
2019-11-20 21:28:32 -0800

    Project creation

---
+++ Testing_CI_CD/Testing_CI_CD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testing_CI_CD
+//
+//  Created by Ranjith vanaparthi on 11/20/19.
+//  Copyright © 2019 Ranjith vanaparthi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Testing_CI_CD/Testing_CI_CDTests/Testing_CI_CDTests.swift
@@ -0,0 +1,34 @@
+//
+//  Testing_CI_CDTests.swift
+//  Testing_CI_CDTests
+//
+//  Created by Ranjith vanaparthi on 11/20/19.
+//  Copyright © 2019 Ranjith vanaparthi. All rights reserved.
+//
+
+import XCTest
+@testable import Testing_CI_CD
+
+class Testing_CI_CDTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
